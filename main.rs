c986c13cfc7197c9f1877c7d15db45089237aeb5
S3sha <95747995+S3sha@users.noreply.github.com>
2026-05-06 18:15:05 +0400

    Add files via upload
    Not so Good AI :D

--- main.rs
+++ main.rs
@@ -1,40 +1,35 @@
-use std::io;
 use rand::Rng;
+use std::io;
+use std::thread;
+use std::time::Duration;
 
-use std::cmp::Ordering;
 fn main() {
-    println!("Guess the number");
+    println!("Please ask a question and the not so good AI/lollm will answer ....");
+    loop {
+        println!("\nWhat is your question: \n");
 
-    loop{
-            
-        let secret_number = rand::thread_rng().gen_range(1..=100);
+        let mut input = String::new();
 
+        io::stdin().read_line(&mut input).expect("Failed");
+        let _input = input.trim();
+        if _input.is_empty() {
 
-        println!("Please input your guess.");
+            println!("You didn't even ask anything! Are you testing me? ILL TAKE YOUR JOB!!!!!!!!!!!!!!!");
+            continue; // This skips the rest of the loop and starts over
+}
 
-        let mut guess = String::new();
+        let phrases = [
+        "\n\nWhat a ridicilous question , i dont have time for this.....",
+        "Is it this one? https://www.youtube.com/watch?v=dQw4w9WgXcQ ",
+        "Here you go all the facts you wanted about paint rollers https://www.eagpainting.com/uncategorized/7-things-you-need-to-know-about-paintbrushes-and-rollers-before-painting-your-house/",
+        "\n\n Yea you used your balance or smtn umm please pay on http://notshadylinkatall.com\n\n",
+        "System overload! Just kidding, it's like this: https://www.reddit.com/r/GymMemes/comments/kbhll5/proper_lifting_form/"
+        ];
+        let mut rng = rand::thread_rng();
 
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number){
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-
-        }
+        let index = rng.gen_range(0..phrases.len());
+        println!("Thinking...");
+        thread::sleep(Duration::from_millis(1000));
+        println!("{}", phrases[index]);
     }
 }
